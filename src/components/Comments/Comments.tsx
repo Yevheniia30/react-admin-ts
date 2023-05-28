4108bc7a22d938f7c3a4ae1c6193e44e3ca9d37c
Yevheniia Suska <susskaya86@gmail.com>
2023-05-28 19:22:31 +0300

    routing

---
+++ src/components/Comments/Comments.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from "react";
+
+interface CommentsProps {
+  width: string;
+  height: string;
+}
+
+const Comments: FC<CommentsProps> = ({ width, height }) => {
+  return <ul></ul>;
+};
+
+export default Comments;
